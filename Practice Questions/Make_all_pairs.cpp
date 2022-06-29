6a96962559b6e0f7b9d4cbec2a4b5e4a5a0c9bff
SUDIPTA BASAK <84557979+SudiptaB-0623@users.noreply.github.com>
2022-06-30 00:38:19 +0530

    Update and rename Make_all_pairs.cpp to Practice Questions/Make_all_pairs.cpp

--- Practice Questions/Make_all_pairs.cpp
+++ Practice Questions/Make_all_pairs.cpp
@@ -1,4 +1,20 @@
+/*Problem
+Chef has an array A of length N.
+In one operation, Chef can choose any two distinct indices i, j and either change A_i to A_j or change A_j to A_i.
+Find the minimum number of operations required to make all the elements of the array equal.
+
+Input Format
+First line will contain T, number of test cases. Then the test cases follow.
+First line of each test case consists of an integer N - denoting the size of array A.
+Second line of each test case consists of N space-separated integers denoting the array A.
+	
+Output Format
+For each test case, output the minimum number of operations required to make all the elements equal.
+*/
+
+
 #include<iostream>
+
 using namespace std;
 
 int main()
